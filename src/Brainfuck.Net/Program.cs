ee430256cac4685cfac0f9888fc39a538224f9e3
uttne <uttne.mkr@gmail.com>
2019-07-07 00:55:32 +0900

    Add args analysis.

--- src/Brainfuck.Net/Program.cs
+++ src/Brainfuck.Net/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using Brainfuck.Net.ArgsAnalysis;
 using Brainfuck.Net.Core;
+using Brainfuck.Net.Core.Tokens;
 
 namespace Brainfuck.Net
 {
@@ -8,24 +11,63 @@ namespace Brainfuck.Net
     {
         static void Main(string[] args)
         {
-            //Todo Analyse args.
-            
-            var src = "+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-." +
-                      "------------.<++++++++.--------.+++.------.--------.>+.#";
-            var parser = new BrainfuckParser();
-            using (var sr = new StringReader(src))
+            var argsParser = new ArgsParser<Options>();
+
+            IArgsData<Options> options; 
+            try
             {
-                var tokens = parser.Parse(sr);
-
-                var memoryTape = new BrainfuckMemoryTape();
-
-                var stream = new BrainfuckStream();
-                
-                var steps = BrainfuckSteps.Create(tokens, memoryTape, stream);
-                
-                steps.Run();
+                options = argsParser.Parse(args);
             }
+            catch (AnalysisException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            if (options.Option.Help)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (options.Has(x => x.Code) && options.Has(x => x.Source))
+            {
+                throw new NotImplementedException();
+            }
+
+            string src;
             
+            
+            var parser = new BrainfuckParser();
+            IEnumerable<BrainfuckToken> tokens;
+            if (options.Has(x => x.Code))
+            {
+                src = options.Option.Code;
+                using (var sr = new StringReader(src))
+                {
+                    tokens = parser.Parse(sr);
+                }
+            }
+            else if (options.Has(x=>x.Source))
+            {
+                using (var fs = new FileStream(options.Option.Source, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using(var sr = new StreamReader(fs))
+                {
+                    tokens = parser.Parse(sr);
+                }
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            var memoryTape = new BrainfuckMemoryTape();
+
+            var stream = new BrainfuckStream();
+
+            var steps = BrainfuckSteps.Create(tokens, memoryTape, stream);
+
+            steps.Run();
+
         }
     }
 }
